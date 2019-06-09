f1a4f7896609943b9128f096e6bb1aa7e06bd115
JYJiangYu <1056160293@qq.com>
2019-06-09 22:50:24 +0800

    springboot_sell第二次提交

--- src/main/java/com/decent/springboot_sell/SpringbootSellApplication.java
+++ src/main/java/com/decent/springboot_sell/SpringbootSellApplication.java
@@ -2,7 +2,7 @@ package com.decent.springboot_sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//emm...新增了一点注释
 @SpringBootApplication
 public class SpringbootSellApplication {
 
